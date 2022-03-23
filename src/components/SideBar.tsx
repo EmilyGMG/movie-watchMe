7bbb50609a79d31031585fba3d1c6a3235a2d35c
EmilyGMG <emily.maria2010@hotmail.com>
2022-03-23 08:40:02 -0300

    done

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,19 +1,22 @@
+import { memo } from "react";
 import { Button } from "./Button";
 
-interface SideBarProps {
-  genres: Array<{
-    id: number;
-    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-    title: string;
-  }>;
-  selectedGenreId: number;
-  buttonClickCallback: (args: any) => void;
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
 }
 
-export function SideBar({
+interface SideBarProps {
+  genres: GenreResponseProps[]
+  selectedGenreId: number;
+  handleClickButton: (id: number) => void;
+}
+
+export function SideBarComponent({
   genres,
   selectedGenreId,
-  buttonClickCallback
+  handleClickButton
 }: SideBarProps) {
   return (
     <nav className="sidebar">
@@ -25,7 +28,7 @@ export function SideBar({
             key={String(genre.id)}
             title={genre.title}
             iconName={genre.name}
-            onClick={() => buttonClickCallback(genre.id)}
+            onClick={() => handleClickButton(genre.id)}
             selected={selectedGenreId === genre.id}
           />
         ))}
@@ -34,3 +37,7 @@ export function SideBar({
     </nav>
   )
 }
+
+export const SideBar = memo(SideBarComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.genres, nextProps)
+})
